86c10e8b0d62054b2967effce3118e1bc0c7ca13
Robzlegz <robzlegz@gmail.com>
2021-01-30 23:35:29 +0200

    creating stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css";
+import Message from './Message';
 
 const App = () => {
+
+  const [username, setUsername] = useState("");
+
+  const hideForm = (e) => {
+    e.preventDefault();
+    document.querySelector("form").classList.add("noform");
+    document.querySelector(".chatContainer").classList.add("openChat");
+  } 
+
   return (
     <div className="app">
-      
+      <form>
+        <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username"/>
+        <button onClick={hideForm} type="submit">Enter</button>
+      </form>
+      <div className="chatContainer">
+        <div className="header">
+          <h1>ChatUp</h1>
+          <h2>Ask Web Community</h2>
+        </div>
+        <div className="chatBody">
+          <Message />
+        </div>
+      </div>
     </div>
   );
 }
